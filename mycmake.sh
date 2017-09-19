7875ed1d0c2262c2c5274914f273b0bb1f49e7c5
JeremyMcCormick <jermccormick@gmail.com>
2017-09-18 22:50:49 -0700

    Example cmake config file.

---
+++ mycmake.sh
@@ -0,0 +1,7 @@
+depdir=/u/ey/jeremym/hps-dev/
+cmake -DCMAKE_INSTALL_PREFIX=../install \
+        -DGDML_DIR=$depdir/gdml/install \
+        -DLCDD_DIR=$depdir/lcdd/install \
+        -DXERCES_LIBRARY=$depdir/xercesc/xerces-c-3.2.0-install/lib/libxerces-c.so -DXERCES_INCLUDE_DIR=$depdir/xercesc/xerces-c-3.2.0-install/include \
+        -DGeant4_DIR=$depdir/geant4/geant4.10.03.p02-install/lib64/Geant4-10.3.2/ \
+        -DLCIO_DIR=$depdir/LCIO/install ..
