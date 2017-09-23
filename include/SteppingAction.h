cc32084d765a3d61f437c29b5b3b2c677a906fc0
JeremyMcCormick <jermccormick@gmail.com>
2017-09-22 19:15:47 -0700

    Add user stepping action within plugin hook.

---
+++ include/SteppingAction.h
@@ -0,0 +1,24 @@
+#ifndef HPSSIM_STEPPINGACTION_H_
+#define HPSSIM_STEPPINGACTION_H_
+
+#include "G4UserSteppingAction.hh"
+
+#include "PluginManager.h"
+
+namespace hpssim {
+
+class SteppingAction : public G4UserSteppingAction {
+
+    public:
+
+        virtual ~SteppingAction() {
+        }
+
+        void UserSteppingAction(const G4Step* aStep) {
+            PluginManager::getPluginManager()->stepping(aStep);
+        }
+};
+
+}
+
+#endif
