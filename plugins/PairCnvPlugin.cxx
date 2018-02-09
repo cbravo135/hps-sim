d455f47a9397bc349fe65ac19c699956aa2f50e6
JeremyMcCormick <jermccormick@gmail.com>
2018-02-09 13:54:00 -0800

    Changed indentation to match other files; other miscellaneous changes.

--- plugins/PairCnvPlugin.cxx
+++ plugins/PairCnvPlugin.cxx
@@ -1,4 +1,3 @@
-// LDMX
 #include "SimPlugin.h"
 
 #include "PrimaryGeneratorAction.h"
@@ -10,131 +9,163 @@
 
 namespace hpssim {
 
-    class PairCnvPlugin : public SimPlugin {
+class PairCnvPlugin : public SimPlugin {
 
-        public:
+    public:
 
-            PairCnvPlugin() {
-                messenger_ = new SimPluginMessenger(this);
-                mgr_ = UserTrackingAction::getUserTrackingAction()->getTrackingManager();
+        PairCnvPlugin() {
+            messenger_ = new SimPluginMessenger(this);
+            mgr_ = UserTrackingAction::getUserTrackingAction()->getTrackingManager();
+        }
 
+        virtual ~PairCnvPlugin() {
+            delete messenger_;
+        }
+
+        std::string getName() {
+            return "PairCnvPlugin";
+        }
+
+        std::vector<PluginAction> getActions() {
+            return {PluginAction::EVENT, PluginAction::TRACKING, PluginAction::STEPPING, PluginAction::RUN};
+        }
+
+        G4ClassificationOfNewTrack stackingClassifyNewTrack(const G4Track* track,
+                const G4ClassificationOfNewTrack& currentTrackClass) {
+            if (!(track->GetDynamicParticle()->GetPrimaryParticle()
+                    && track->GetParticleDefinition()->GetPDGEncoding() == 22)) {
+                return fWaiting;
+            }
+            return currentTrackClass;
+        }
+
+        void beginRun(const G4Run* run) {
+
+            if (PrimaryGeneratorAction::getPrimaryGeneratorAction()->getGenerators().size() > 1) {
+                G4Exception("", "", FatalException, "This plugin cannot run when using multiple event generators.");
             }
 
-            virtual ~PairCnvPlugin() {
-                delete messenger_;
-            }
+            volumes_.clear();
 
-            virtual std::string getName() {
-                return "PairCnvPlugin";
-            }
+            // FIXME: hard-coded volume names; need a custom messenger command to set the volume names
+            addVolume("target_vol");
+            addVolume("module_L1b_halfmodule_stereo_sensor_volume");
+            addVolume("module_L1b_halfmodule_axial_sensor_volume");
+            addVolume("module_L1t_halfmodule_stereo_sensor_volume");
+            addVolume("module_L1t_halfmodule_axial_sensor_volume");
+            addVolume("module_L2b_halfmodule_stereo_sensor_volume");
+            addVolume("module_L2b_halfmodule_axial_sensor_volume");
+            addVolume("module_L2t_halfmodule_stereo_sensor_volume");
+            addVolume("module_L2t_halfmodule_axial_sensor_volume");
+        }
 
-            std::vector<PluginAction> getActions() {
-                return {PluginAction::EVENT, PluginAction::TRACKING, PluginAction::STEPPING, PluginAction::RUN};
-            }
-
-            void beginRun(const G4Run* run) {
-
-                if (PrimaryGeneratorAction::getPrimaryGeneratorAction()->getGenerators().size() > 1) {
-                    G4Exception("", "", FatalException, "This plugin cannot run with multiple event generators.");
-                }
-
-                auto pvStore = G4PhysicalVolumeStore::GetInstance();
-                volumes_.clear();
-
-                // FIXME: hard-coded volume names; need custom messenger to set vol names
-                addVolume("module_L1b_halfmodule_stereo_sensor_volume");
-                addVolume("module_L1b_halfmodule_axial_sensor_volume");
-                addVolume("module_L1t_halfmodule_stereo_sensor_volume");
-                addVolume("module_L1t_halfmodule_axial_sensor_volume");
-                addVolume("module_L2b_halfmodule_stereo_sensor_volume");
-                addVolume("module_L2b_halfmodule_axial_sensor_volume");
-                addVolume("module_L2t_halfmodule_stereo_sensor_volume");
-                addVolume("module_L2t_halfmodule_axial_sensor_volume");
-            }
-
-            void postTracking(const G4Track* aTrack) {
-                if (aTrack->GetDynamicParticle()->GetPrimaryParticle()) {
-                    if (aTrack->GetDynamicParticle()->GetPDGcode() == 22) {
-                        if (!cnvFlag_) {
-                            auto anEvent = const_cast<G4Event*>(G4RunManager::GetRunManager()->GetCurrentEvent());
-                            const_cast<G4Event*>(anEvent)->SetEventAborted();
-                        }
+        void postTracking(const G4Track* aTrack) {
+            if (aTrack->GetDynamicParticle()->GetPrimaryParticle()) {
+                if (aTrack->GetDynamicParticle()->GetPDGcode() == 22) {
+                    if (!cnvFlag_) {
+                        auto anEvent = const_cast<G4Event*>(G4RunManager::GetRunManager()->GetCurrentEvent());
+                        const_cast<G4Event*>(anEvent)->SetEventAborted();
                     }
                 }
             }
+        }
 
-            void stepping(const G4Step* step) {
-                auto track = step->GetTrack();
-                auto pdef = track->GetParticleDefinition();
-                if (track->GetDynamicParticle()->GetPrimaryParticle() && pdef->GetPDGEncoding() == 22) {
-                    auto secos = step->GetSecondaryInCurrentStep();
-                    if (secos->size()) {
-                        if (inVolumes(step->GetPreStepPoint()->GetTouchable())) {
-                            for (auto seco : *secos) {
-                                if (seco->GetCreatorProcess()) {
-                                    auto proc = seco->GetCreatorProcess();
-                                    auto pv = step->GetPreStepPoint()->GetPhysicalVolume();
+        void stepping(const G4Step* step) {
+            auto track = step->GetTrack();
+            auto pdef = track->GetParticleDefinition();
+            if (!cnvFlag_ && track->GetDynamicParticle()->GetPrimaryParticle() && pdef->GetPDGEncoding() == 22) {
+                auto secos = step->GetSecondaryInCurrentStep();
+                if (secos->size()) {
+                    if (inVolumes(step->GetPreStepPoint()->GetTouchable())) {
+                        for (auto seco : *secos) {
+                            if (seco->GetCreatorProcess()) {
+                                auto proc = seco->GetCreatorProcess();
+                                auto pv = step->GetPreStepPoint()->GetPhysicalVolume();
+                                if (proc->GetProcessName() == "conv") {
                                     if (verbose_ > 1) {
+                                        std::cout << "PairCnvPlugin: seco created by proc '" << proc->GetProcessName()
+                                                << "' in pv '" << pv->GetName() << "'" << std::endl;
                                     }
-                                    if (proc->GetProcessName() == "conv") {
-                                        if (verbose_ > 1) {
-                                            std::cout << "PairCnvPlugin: seco created by proc '" << proc->GetProcessName() 
-                                                    << "' in pv '" << pv->GetName() << "'" << std::endl;
-                                        }
-                                        cnvFlag_ = true;
-                                    }
+                                    cnvFlag_ = true;
+                                    break;
                                 }
                             }
                         }
                     }
                 }
             }
+        }
 
-            bool inVolumes(const G4VTouchable* touchable) {
-                int depth = touchable->GetHistoryDepth();
-                for (int iDepth = depth; iDepth >= 0; iDepth--) {
-                    G4VPhysicalVolume* pv = touchable->GetVolume(iDepth);
-                    if (pv) {
-                        std::cout << pv->GetName() << std::endl;
-                        if(std::find(volumes_.begin(), volumes_.end(), pv) != volumes_.end()) {
-                            std::cout << "PairCnvPlugin: found vol '" << pv->GetName() << "' in list." << std::endl;
-                            return true;
-                        }
+        bool inVolumes(const G4VTouchable* touchable) {
+            int depth = touchable->GetHistoryDepth();
+            for (int iDepth = depth; iDepth >= 0; iDepth--) {
+                G4VPhysicalVolume* pv = touchable->GetVolume(iDepth);
+                if (pv) {
+                    //std::cout << pv->GetName() << std::endl;
+                    if(std::find(volumes_.begin(), volumes_.end(), pv) != volumes_.end()) {
+                        std::cout << "PairCnvPlugin: found vol '" << pv->GetName() << "' in list." << std::endl;
+                        return true;
                     }
                 }
-                return false;
             }
+            return false;
+        }
 
-            void addVolume(std::string volName) {
-                auto vol = G4PhysicalVolumeStore::GetInstance()->GetVolume(volName);
-                if (vol) {
-                    volumes_.push_back(G4PhysicalVolumeStore::GetInstance()->GetVolume(volName));
-                } else {
-                    G4Exception("", "", FatalException, "Volume not found in PV store.");
+        void addVolume(std::string volName) {
+            auto vol = G4PhysicalVolumeStore::GetInstance()->GetVolume(volName);
+            if (vol) {
+                volumes_.push_back(G4PhysicalVolumeStore::GetInstance()->GetVolume(volName));
+            } else {
+                G4Exception("", "", FatalException, "Volume not found in PV store.");
+            }
+        }
+
+        void endEvent(const G4Event* event) {
+            auto generators = PrimaryGeneratorAction::getPrimaryGeneratorAction()->getGenerators();
+            std::cout << "PairCnvPlugin: Setting read flag to " << cnvFlag_ << std::endl;
+            generators[0]->setReadFlag(cnvFlag_);
+            if (cnvFlag_) {
+                // Conversion occurred so propagate event weight.
+                auto anEvent = const_cast<G4Event*>(G4RunManager::GetRunManager()->GetCurrentEvent());
+                float weight = 1.0 / nEventsRead_;
+                anEvent->GetPrimaryVertex()->SetWeight(1.0 / nEventsRead_);
+                if (verbose_ > 1) {
+                    std::cout << "PairCnvPlugin: Set weight to " << weight << " from " << nEventsRead_ << " events generated." << std::endl;
+                }
+            } else {
+                // Reached maximum number of events so reset plugin state and read next generator event.
+                nEventsRead_ += 1;
+                if (nEventsRead_ > maxEvents_) {
+                    std::cout << "PairCnvPlugin: Reread " << nEventsRead_ << " generator events without pair cnv; reading next input event." << std::endl;
+                    nEventsRead_ = 1;
+                    generators[0]->setReadFlag(true);
                 }
             }
+        }
 
-            void endEvent(const G4Event* event) {
-                auto generators = PrimaryGeneratorAction::getPrimaryGeneratorAction()->getGenerators();
-                std::cout << "PairCnvPlugin: Setting read flag to " << cnvFlag_ << std::endl;
-                generators[0]->setReadFlag(cnvFlag_);
+        void beginEvent(const G4Event*) {
+            if (cnvFlag_) {
+                nEventsRead_ = 1;
             }
+            cnvFlag_ = false;
+        }
 
-            void beginEvent(const G4Event*) {
-                cnvFlag_ = false;
-            }
+    private:
 
-        private:
+        /* List of physical volumes to check for conversions. */
+        std::vector<G4VPhysicalVolume*> volumes_;
 
-            /* List of physical volume names where conversions may occur. */
-            std::vector<G4VPhysicalVolume*> volumes_;
+        /* Flag indicating if conversion occurred; reset for every event. */
+        bool cnvFlag_{false};
 
-            bool cnvFlag_{false};
+        G4UImessenger* messenger_;
 
-            G4UImessenger* messenger_;
+        G4TrackingManager* mgr_;
 
-            G4TrackingManager* mgr_;
-    };
+        int nEventsRead_{1};
+
+        int maxEvents_{1000};
+};
 }
 
 SIM_PLUGIN(hpssim, PairCnvPlugin)
